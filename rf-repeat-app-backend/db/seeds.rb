6430987e73dd0dd075f569baab1a6dcda1bf9a15
koukikubo <kouki.xx.0817@gmail.com>
2026-03-10 20:11:44 +0900

    RFロジック検証用のseedデータ作成 #4

--- rf-repeat-app-backend/db/seeds.rb
+++ rf-repeat-app-backend/db/seeds.rb
@@ -1,9 +1,28 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# ---------------------
+# customersのダミーデータを作成
+# ---------------------
+
+customers = []
+
+20.times do |i|
+  customers << Customer.create!(
+    name: "顧客#{i + 1}"
+  )
+end
+
+puts "customers created"
+
+# ---------------------
+# reservationsのダミーデータを作成
+# ---------------------
+
+20.times do
+  Reservation.create!(
+    customer: customers.sample,
+    visited_at: rand(1..180).days.ago
+  )
+end
+
+puts "reservations created"
+
+puts "seed finished"
