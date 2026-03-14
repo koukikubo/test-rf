def2672d8a3dc8bf521733797959b020e077d66b
koukikubo <kouki.xx.0817@gmail.com>
2026-03-14 14:02:08 +0900

    顧客登録APIと正常系テストを追加 #20

---
+++ rf-repeat-app-backend/spec/requests/api/v1/customers_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+# 正常系-顧客作成APIのリクエストテスト
+RSpec.describe "Api::V1::Customers", type: :request do
+  describe "POST /api/v1/customers" do
+    it "顧客を作成できる" do
+      expect {
+        post '/api/v1/customers', params: { customer: { name: "山田太郎" } }
+      }
+      .to change(Customer, :count).by(1)
+
+      expect(response).to have_http_status(:created)
+
+      json = JSON.parse(response.body)
+
+      expect(json["name"]).to eq("山田太郎")
+    end
+  end
+end
