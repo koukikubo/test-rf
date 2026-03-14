a23ebe3233004993767bfc1813051afd1e9701ee
koukikubo <kouki.xx.0817@gmail.com>
2026-03-14 14:19:24 +0900

    ✅ 顧客登録APIの異常系テストを追加 #20

--- rf-repeat-app-backend/spec/requests/api/v1/customers_spec.rb
+++ rf-repeat-app-backend/spec/requests/api/v1/customers_spec.rb
@@ -14,5 +14,16 @@ RSpec.describe "Api::V1::Customers", type: :request do
 
       expect(json["name"]).to eq("山田太郎")
     end
+    it "名前がない場合は作成できない" do
+      expect {
+        post '/api/v1/customers', params: { customer: { name: "" } }
+      }.not_to change(Customer, :count)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      json = JSON.parse(response.body)
+
+      expect(json["errors"]).to include("Nameを入力してください")
+    end
   end
 end
