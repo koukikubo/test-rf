fb4de6045b4efe1f02826ca9d5139d7199bfc3f1
koukikubo <kouki.xx.0817@gmail.com>
2026-03-14 17:58:59 +0900

    ✨ 顧客選択式の予約登録画面を作成 #21

---
+++ rf-repeat-app-frontend/app/reservations/new/page.tsx
@@ -0,0 +1,10 @@
+import ReservationForm from "@/components/customers/reservations/reservation-form";
+
+export default async function NewReservationPage() {
+  return (
+    <main>
+      <h1>New Reservation</h1>
+      <ReservationForm />
+    </main>
+  );
+}
