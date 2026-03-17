a2e1a48241b9792fe22dd6d9eef3207956b00799
koukikubo <kouki.xx.0817@gmail.com>
2026-03-17 20:38:21 +0900

    ✨ RfMasterモデルにバリデーションを追加

--- rf-repeat-app-backend/app/models/rf_master.rb
+++ rf-repeat-app-backend/app/models/rf_master.rb
@@ -1,2 +1,6 @@
 class RfMaster < ApplicationRecord
+  validates :rank, presence: true, uniqueness: true
+  validates :min_visit_count, presence: true
+  validates :min_days_since_last_visit, presence: true
+  validates :position, presence: true, uniqueness: true
 end
