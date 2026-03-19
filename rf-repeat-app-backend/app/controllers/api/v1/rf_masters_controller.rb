1f483c0b6f666756bcbe27ae4006d978a0dc5adc
koukikubo <kouki.xx.0817@gmail.com>
2026-03-20 01:10:06 +0900

    🔧 RfMastersコントローラーからcreateおよびdestroyアクションを削除し、ルーティングを更新

--- rf-repeat-app-backend/app/controllers/api/v1/rf_masters_controller.rb
+++ rf-repeat-app-backend/app/controllers/api/v1/rf_masters_controller.rb
@@ -9,16 +9,6 @@ class Api::V1::RfMastersController < ApplicationController
     render json: rf_master, status: :ok
   end
 
-  def create
-    rf_master = RfMaster.new(rf_master_params)  
-
-    if rf_master.save
-      render json: rf_master, status: :created
-    else
-      render json: { errors: rf_master.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
   def update
     rf_master = RfMaster.find(params[:id])
 
@@ -27,12 +17,6 @@ class Api::V1::RfMastersController < ApplicationController
     else
       render json: { errors: rf_master.errors.full_messages }, status: :unprocessable_entity
     end
-  end
-
-  def destroy
-    rf_master = RfMaster.find(params[:id])
-    rf_master.destroy
-    head :no_content
   end
 
   private
