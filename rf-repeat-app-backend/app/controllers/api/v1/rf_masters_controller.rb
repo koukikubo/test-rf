0012b34bf0833ea3cdbf1aa15d27bd2b28f73c41
koukikubo <kouki.xx.0817@gmail.com>
2026-03-21 13:07:39 +0900

    ✨ RfRankCalculatorにランク計算ロジックを追加し、更新機能を強化

--- rf-repeat-app-backend/app/controllers/api/v1/rf_masters_controller.rb
+++ rf-repeat-app-backend/app/controllers/api/v1/rf_masters_controller.rb
@@ -13,6 +13,7 @@ class Api::V1::RfMastersController < ApplicationController
     rf_master = RfMaster.find(params[:id])
 
     if rf_master.update(rf_master_params)
+      RfRankCalculator.call
       render json: rf_master, status: :ok
     else
       render json: { errors: rf_master.errors.full_messages }, status: :unprocessable_entity
@@ -22,6 +23,11 @@ class Api::V1::RfMastersController < ApplicationController
   private
   
   def rf_master_params
-    params.require(:rf_master).permit(:rank, :min_visit_count, :min_days_since_last_visit, :position)
+    params.require(:rf_master).permit(
+      :rank,
+      :min_visit_count,
+      :max_visit_count,
+      :position
+      )
   end
 end
