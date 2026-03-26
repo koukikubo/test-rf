749ff57fd1bb92c4047becad07e98a2cc228b91f
koukikubo <kouki.xx.0817@gmail.com>
2026-03-26 23:21:35 +0900

    ✨ 予約操作時にRFスコア更新Jobを追加

--- rf-repeat-app-backend/app/controllers/api/v1/reservations_controller.rb
+++ rf-repeat-app-backend/app/controllers/api/v1/reservations_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::ReservationsController < ApplicationController
     reservation = Reservation.new(reservation_params)
 
     if reservation.save
-      
+      RfScoreUpdateJob.perform_later(reservation.customer_id)
       render json: reservation, status: :created
     else
       render json: { errors: reservation.errors.full_messages }, status: :unprocessable_entity
@@ -32,9 +32,17 @@ class Api::V1::ReservationsController < ApplicationController
 
   def update
     reservation = Reservation.find(params[:id])
-
+    # 更新前の顧客IDを退避しておく
+    # customer_id が変更された場合、変更前の顧客も再計算が必要になるため
+    old_customer_id = reservation.customer_id
     if reservation.update(reservation_params)
-      render json: reservation, status: :ok
+      # もし顧客IDが変わっていたら、変更前の顧客も再計算する
+      if old_customer_id != reservation.customer_id
+        RfScoreUpdateJob.perform_later(old_customer_id)
+      end
+
+      # 更新後の顧客についても再計算する
+      RfScoreUpdateJob.perform_later(reservation.customer_id)
     else
       render json: { errors: reservation.errors.full_messages }, status: :unprocessable_entity
     end
@@ -42,11 +50,20 @@ class Api::V1::ReservationsController < ApplicationController
 
   def destroy
     reservation = Reservation.find(params[:id])
-    reservation.destroy
-    render json: { message: "Reservation deleted successfully" }, status: :ok
+    # 削除後は reservation.customer_id を安全に使えない可能性があるため、
+    # 先に顧客IDを退避しておく
+    customer_id = reservation.customer_id
+
+    if reservation.destroy
+      RfScoreUpdateJob.perform_later(customer_id)
+      render json: { message: "Reservation deleted successfully" }, status: :ok
+    else
+      render json: { errors: reservation.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
+  
   def reservation_params
     params.require(:reservation).permit(:customer_id, :visited_at)
   end
