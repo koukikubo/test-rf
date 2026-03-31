7ee1fad0ee2218de1d2be8070cf894cc5261bfe7
koukikubo <kouki.xx.0817@gmail.com>
2026-03-31 19:19:13 +0900

    🔥 不要なファイルの削除

--- rf-repeat-app-backend/app/controllers/api/v1/rfm_matrices_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::RfmMatricesController < ApplicationController
-  def index
-    # RfmMatrixServiceを呼び出して、RFMマトリクスのデータを取得
-    render json: RfmMatrixBuilder.call, status: :ok 
-  end
-end
