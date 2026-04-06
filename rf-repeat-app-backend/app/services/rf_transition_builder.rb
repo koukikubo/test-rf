5690be24c86ffc7603e7b720c65a4d86b94dacf2
koukikubo <kouki.xx.0817@gmail.com>
2026-04-06 18:47:22 +0900

    ♻️ タイポの修正とコメントの付与

--- rf-repeat-app-backend/app/services/rf_transition_builder.rb
+++ rf-repeat-app-backend/app/services/rf_transition_builder.rb
@@ -19,10 +19,10 @@ class RfTransitionBuilder
     current_base_month = base_month
     # 分析の基準となる月の末日を求める関数
     current_base_date = current_base_month.end_of_month
-    # 分析の基準となる月の1年前の末日を求める関数
+    # 分析基準となる月と同じ月のラベルと、1年前の同じ月のラベルを作る関数
     previous_base_date = (current_base_month - 1.month).end_of_month
 
-    # 分析の基準となる月と同じ月のラベルと、1年前の同じ月のラベルを作る関数
+    # 分析基準となる月と同じ月のラベルと、1年前の同じ月のラベルを作る関数
     current_month_label = current_base_month.strftime("%Y年%m月")
     previous_month_label = (current_base_month - 1.month).strftime("%Y年%m月")
 
@@ -30,16 +30,16 @@ class RfTransitionBuilder
     period = aggregation_period_range(current_base_month)
 
     # 分析の基準となる月から5年前の月の範囲内の予約を取得する関数
-    reservations = Reservation.where(visit_at: period[:start]..period[:end])
+    reservations = Reservation.where(visited_at: period[:start]..period[:end])
     customer_ids = reservations.select(:customer_id).distinct.pluck(:customer_id)
 
     current_counts = Hash.new(0)
     previous_counts = Hash.new(0)
 
-    # 予約情報をもとに、行と列のキーを判定して、セルの集計を行う関数
+    # 分析の基準となる月から5年前の月の範囲内の予約をした顧客ごとに、行と列のキーを判定して、セルのハッシュを更新する関数
     customer_ids.each do |customer_id|
       customer_reservations = reservations.where(customer_id: customer_id)
-      
+      # 分析の基準となる月の末日を基準にして、行と列のキーを判定する関数
       current_result = RfRankRule.call(
         reservations: customer_reservations,
         base_date: current_base_date
@@ -53,14 +53,15 @@ class RfTransitionBuilder
       current_counts[current_result[:rank]] += 1
       previous_counts[previous_result[:rank]] += 1
     end
-
+    # セルのハッシュをもとに、行と列のキーを判定して、差分の数値と率を計算する関数
     current_total = current_counts.values.sum
-
+    # 行と列のキーをもとに、セルのハッシュを更新する関数
     rows = RANK.map do |rank|
       current_count = current_counts[rank[:key]]
       previous_count = previous_counts[rank[:key]]
       diff_count = current_count - previous_count
 
+      # 前回の数値が0の場合は、差分率はnilとする。そうでない場合は、差分の数値を前回の数値で割って100をかけて、小数点第一位までの数値とする。
       diff_rate = 
         if previous_count.zero? 
           nil
@@ -75,6 +76,7 @@ class RfTransitionBuilder
           (current_count.to_f / current_total * 100).round(1)
         end
 
+      # 行と列のキーをもとに、セルのハッシュを更新する関数
       {
         rank_key: rank[:key],
         rank_label: rank[:label],
@@ -85,7 +87,7 @@ class RfTransitionBuilder
         current_percentage: current_percentage
       }
     end
-
+    # 分析の基準となる月と同じ月のラベルと、1年前の同じ月のラベルと、行と列のキーをもとに、セルのハッシュを更新する関数
     {
       current_month_label: current_month_label,
       previous_month_label: previous_month_label,
