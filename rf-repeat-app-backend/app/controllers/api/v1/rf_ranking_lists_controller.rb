9820b0ef5a4a7daf8adc8bafcf7aeaad7660eae7
koukikubo <kouki.xx.0817@gmail.com>
2026-04-14 21:38:29 +0900

    ♻️ テーブル名をRfRankingListへ変更

---
+++ rf-repeat-app-backend/app/controllers/api/v1/rf_ranking_lists_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::RfRankingListsController < ApplicationController
+  def index
+    base_date = params[:base_date].present? ? Date.parse(params[:base_date]) : Time.current.to_date
+
+    # RfRankingListモデルからデータを取得
+    rows = Customer.includes(:reservations).map do |customer|
+      result = RfRankRule.call(
+        reservations: customer.reservations,
+        base_date: base_date
+        ) 
+        {
+        id: customer.id,
+        visit_count: result[:visit_count],
+        last_visit_at: result[:last_visit_at],
+        rank: result[:rank],
+        customer: {
+          id: customer.id,
+          name: customer.name
+        }
+      }
+    end
+
+    rank_order = RfRankMaster.all
+                             .sort_by { |r| r[:order] }
+                             .map { |r| r[:key] }
+                             .each_with_index
+                             .to_h
+
+    rows = rows.sort_by do |row|
+      [
+        rank_order[row[:rank]] || 999,
+        -row[:visit_count].to_i
+      ]
+    end
+
+    render json: rows
+  end
+end
--- rf-repeat-app-backend/config/routes.rb
+++ rf-repeat-app-backend/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :rf_scores, only: [:index]
+      resources :rf_ranking_lists, only: [:index]
       resources :customers, only: [:create, :index, :show] 
       resources :reservations, only: [:create, :index, :show, :update, :destroy]
       resources :rf_transitions, only: [:index]
-      resources :rf_masters, only: [:index, :show, :update]
+      resources :rf_masters, only: [:index]
       resources :rf_rank_summaries, only: [:index]
       resources :rf_rank_fluctuations, only: [:index]
     end
